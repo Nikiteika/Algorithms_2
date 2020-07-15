91c23ec5fb58d325865bf8143312042e0ed5f3b2
Nikita <53521100+Nikiteika@users.noreply.github.com>
2020-07-15 16:01:17 +0300

    Create 10_deep_first_search.py

---
+++ 10_deep_first_search.py
@@ -0,0 +1,66 @@
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+        self.Hit = False
+
+
+class SimpleGraph:
+
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v):
+        try:
+            ind = self.vertex.index(None)
+        except ValueError:
+            return False
+        self.vertex[ind] = Vertex(v)
+        return True
+        # ваш код добавления новой вершины
+        # с значением value
+        # в свободное место массива vertex
+
+        # здесь и далее, параметры v -- индекс вершины
+
+    # в списке  vertex
+    def RemoveVertex(self, v):
+        if 0 <= v < self.max_vertex:
+            self.vertex[v] = None
+            self.m_adjacency[v] = [0] * self.max_vertex
+            for el in self.m_adjacency:
+                el[v] = 0
+            return True
+        else:
+            return False
+        # ваш код удаления вершины со всеми её рёбрами
+
+    def IsEdge(self, v1, v2):
+        if 0 <= v1 < self.max_vertex and 0 <= v2 < self.max_vertex:
+            if self.m_adjacency[v1][v2] == 1 and self.m_adjacency[v2][v1] == 1:
+                return True
+            else:
+                return False
+        else:
+            return False
+        # True если есть ребро между вершинами v1 и v2
+
+    def AddEdge(self, v1, v2):
+        if 0 <= v1 < self.max_vertex and 0 <= v2 < self.max_vertex:
+            self.m_adjacency[v1][v2] = 1
+            self.m_adjacency[v2][v1] = 1
+        # добавление ребра между вершинами v1 и v2
+
+    def RemoveEdge(self, v1, v2):
+        if 0 <= v1 < self.max_vertex and 0 <= v2 < self.max_vertex:
+            self.m_adjacency[v1][v2] = 0
+            self.m_adjacency[v2][v1] = 0
+        # удаление ребра между вершинами v1 и v2
+
+    def DepthFirstSearch(self, VFrom, VTo):
+        pass
+# узлы задаются позициями в списке vertex
+# возвращается список узлов -- путь из VFrom в VTo
+# или [] если пути нету
