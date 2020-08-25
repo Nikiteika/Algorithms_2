6b619838628676fe8284f9ff4a8f996bf4bd9c22
Nikita <53521100+Nikiteika@users.noreply.github.com>
2020-08-25 20:58:50 +0300

    Update 12_weak_verices.py

--- 12_weak_verices.py
+++ 12_weak_verices.py
@@ -16,23 +16,6 @@ class Stack:
         return
 
 
-class Queue:
-    def __init__(self):
-        self.queue = []
-
-    def enqueue(self, item):
-        self.queue.append(item)
-
-    def dequeue(self):
-        if len(self.queue) != 0:
-            return self.queue.pop(0)
-        else:
-            return None
-
-    def size(self):
-        return len(self.queue)
-
-
 class Vertex:
 
     def __init__(self, val):
